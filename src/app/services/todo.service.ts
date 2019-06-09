5f030593a266a0342137ebfe54d0f978f6fc1ae8
Elias Feijó <eliasfeijo.dev@gmail.com>
2019-06-08 23:57:41 -0300

    Using Http Get request to get initial Todos from JSONPlaceholder

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,25 +9,10 @@ import { Injectable } from '@angular/core';
 
 export class TodoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: "aaa",
-        completed: false
-      },
-      {
-        id: 2,
-        title: "bbbb",
-        completed: true
-      },
-      {
-        id: 3,
-        title: "ccccc",
-        completed: false
-      }
-    ];
+  getTodos():Observable<Todo[]> {
+    const url = 'https://jsonplaceholder.typicode.com/todos?_limit=5';
+    return this.http.get<Todo[]>(url);
   }
 }
